22623c61d8c105a1473451840706d680078d1ed3
T-tssxuan <2270360114@qq.com>
2016-09-12 13:09:34 +0800

    [add] Add the Readme and the ubuntu 16.04 install script

---
+++ docker/Readme.md
@@ -0,0 +1,4 @@
+# docker install
+There is some script for install docker in different OS.
+
+1. [ubuntu 16.04 LTS](./ubuntu_16.04.sh)
---
+++ docker/ubuntu_16.04.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# update package information and install CA certifications
+sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates
+
+# add new GPG key
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070A
+
+# add docker repository
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
+
+# update and purge the old one
+sudo apt-get update && sudo apt-get purge lxc-docker
+
+# prerequisites
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+
+# install 
+sudo apt-get update && sudo apt-get install docker-engine && sudo service docker start
+
+# verify
+sudo docker run hello-world
