f2bce5b7960bb296f02637e1aec5a2fe81888fa0
T-tssxuan <2270360114@qq.com>
2016-09-16 15:40:49 +0800

    [alter] change the openvpn default port

--- vpn_openvpn/start_openvpn_server.sh
+++ vpn_openvpn/start_openvpn_server.sh
@@ -1,2 +1,2 @@
 OVPN_DATA="open-data"
-docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
+docker run -v $OVPN_DATA:/etc/openvpn -d -p 21000:21000/udp --cap-add=NET_ADMIN kylemanna/openvpn
