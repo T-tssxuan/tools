b39b81e9826880b409c3f6ad5762f269830319a0
T-tssxuan <2270360114@qq.com>
2016-11-02 23:24:46 +0800

    [new] add note

--- vpn_openvpn/start_openvpn_server.sh
+++ vpn_openvpn/start_openvpn_server.sh
@@ -1,2 +1,3 @@
 OVPN_DATA="open-data"
+# note you should specify it with udp and the port in your client certification
 docker run -v $OVPN_DATA:/etc/openvpn -d -p 21000:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
